53880e34908aabcd70f6db9a4eff95d21365a0f2
Youssef Ghonem <youssef.fcaih@gmail.com>
2026-01-17 10:10:39 +0200

    handle reset code

--- src/Contracting.Application/Features/Users/Commands/ResetPasswordCommand/ResetPasswordCommand.cs
+++ src/Contracting.Application/Features/Users/Commands/ResetPasswordCommand/ResetPasswordCommand.cs
@@ -3,6 +3,6 @@ using MediatR;
 
 namespace Contracting.Application.Features.Users.Commands.ResetPasswordCommand;
 
-public record ResetPasswordRequest(string Email, string Token, string NewPassword, string ConfirmPassword);
+public record ResetPasswordRequest(string Email, string Code, string NewPassword, string ConfirmPassword);
 
 public record ResetPasswordCommand(ResetPasswordRequest Request) : IRequest<ErrorOr<string>>;
