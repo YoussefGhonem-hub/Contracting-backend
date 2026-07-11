395ff27d5cbe3e359d3a247176ad03745e8bf0ce
amr-sherif <amrshrif53@gmail.com>
2026-07-11 17:05:12 +0300

    fixes

--- src/Contracting.Shared/Dtos/HelperDtos/NotificationDtos.cs
+++ src/Contracting.Shared/Dtos/HelperDtos/NotificationDtos.cs
@@ -12,6 +12,7 @@ namespace Contracting.Shared.Dtos.HelperDtos
         public Guid? DepartmentId { get; set; }
         public Guid? RequestId { get; set; }
         public string? RequestTitle { get; set; }
+        public string? RequestType { get; set; }
         public Guid? ChatGroupId { get; set; }
         public string? Type { get; set; }
         public DateTimeOffset SentAt { get; set; }
